38874fc48c4123c83342203c8d9cf206cc2f53e6
Mohammadhossein Salari <mohammadhossein.salari@gmail.com>
2026-04-01 01:15:11 +0300

    fix: deploy script branch name and color updates

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -e
 python3 build.py
 
 cd public
-git init
+git init -b gh-pages
 git add -A
 git commit -m "deploy"
-git push --force git@github.com:sandoogh/hafez-fal.ir.git master:gh-pages
+git push --force git@github.com:sandoogh/hafez-fal.ir.git gh-pages
 
 echo "Deployed to gh-pages"
